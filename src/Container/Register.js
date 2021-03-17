96f84439695f5f285e865085ad554c40d7f1c308
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2021-03-17 19:21:39 +0545

    Commit to push into remote

--- src/Container/Register.js
+++ src/Container/Register.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import { error } from 'jquery';
+import { error, event } from 'jquery';
 class Register extends Component{
 
     state = {
@@ -38,18 +38,46 @@ class Register extends Component{
                 </h1>
                 <form>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <label for="inputFirstName">FirstName</label>
+                        <p><input type="text" class="form-control" id="firstName" placeholder="FirstName" 
+                        value={this.state.firstName}
+                        onChange = {(event)=>{this.setState({firstName:event.target.value})}}/></p>
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                        <label for="lastName">lastName</label>
+                        <p><input type="text" class="form-control" id="lastName" placeholder="LastName" 
+                        value={this.state.lastName}
+                        onChange = {(event)=>{this.setState({lastName:event.target.value})}}/></p>
                     </div>
-                    <div class="form-group form-check">
+                    <div class="form-group">
+                        <label for="dob">Date of Birth</label>
+                        <input type="date" class="form-control" id="dateofbirth" placeholder="Date of Birth"
+                        value={this.state.dob}
+                        onChange = {(event)=>{this.setState({dob:event.target.value})}}/>
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" id="username" placeholder="UserName"
+                        value={this.state.userName}
+                        onChange = {(event)=>{this.setState({userName:event.target.value})}}/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Email"
+                        value={this.state.email}
+                        onChange = {(event)=>{this.setState({email:event.target.value})}}/>
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>                    
+                    <div class="form-group">
+                        <label for="Password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password"
+                        value={this.state.password}
+                        onChange = {(event)=>{this.setState({password:event.target.value})}}/>
+                    </div>
+                    {/* <div class="form-group form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
+                    </div> */}
                     <button type="submit" class="btn btn-primary" onClick={this.sendUserData}>Register</button>
                 </form>
             </div>
