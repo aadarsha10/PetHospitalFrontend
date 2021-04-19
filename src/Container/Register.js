ae619eac3968c1a3a6f4499e360eb2c2a8ebab5f
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2021-04-19 21:18:49 +0545

    changes to registration page

--- src/Container/Register.js
+++ src/Container/Register.js
@@ -8,7 +8,7 @@ class Register extends Component{
         lastName : "",
         userName : "",
         email : "",
-        password : "",
+        password : ""
     }
     sendUserData = (e) =>{
         e.preventDefault();
@@ -19,7 +19,7 @@ class Register extends Component{
             email : this.state.email,
             password : this.state.password
         }
-        axios.post("https://localhost:90/registerUser",data)
+        axios.post("https://localhost:90/register",data)
         .then(response=>{
             console.log(response)
         })
@@ -31,50 +31,56 @@ class Register extends Component{
     render(){
     return (
         <div>
-                <h1>
-                Registration form
-                </h1>
-                <form>
-                    <div class="form-group">
-                        <label for="inputFirstName">FirstName</label>
-                        <p><input type="text" class="form-control" id="firstName" placeholder="FirstName" 
-                        value={this.state.firstName}
-                        onChange = {(event)=>{this.setState({firstName:event.target.value})}}/></p>
-                    </div>
-                    <div class="form-group">
-                        <label for="lastName">lastName</label>
-                        <p><input type="text" class="form-control" id="lastName" placeholder="LastName" 
-                        value={this.state.lastName}
-                        onChange = {(event)=>{this.setState({lastName:event.target.value})}}/></p>
-                    </div>
-                    <div class="form-group">
-                        <label for="dob">Date of Birth</label>
-                        <input type="date" class="form-control" id="dateofbirth" placeholder="Date of Birth"
-                        value={this.state.dob}
-                        onChange = {(event)=>{this.setState({dob:event.target.value})}}/>
-                    </div>
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" placeholder="UserName"
-                        value={this.state.userName}
-                        onChange = {(event)=>{this.setState({userName:event.target.value})}}/>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputEmail">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Email"
-                        value={this.state.email}
-                        onChange = {(event)=>{this.setState({email:event.target.value})}}/>
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>                    
-                    <div class="form-group">
-                        <label for="Password">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Password"
-                        value={this.state.password}
-                        onChange = {(event)=>{this.setState({password:event.target.value})}}/>
-                    </div>
-                    <button type="submit" class="btn btn-primary" onClick={this.sendUserData}>Register</button>
-                </form>
+        <div className="container">
+            <h1 className="text-center">User Registration</h1>
+
+            <div className="row">
+
+                <div className='col-md-6' id="regBox">
+                    <form id="registrationForm" method="POST">
+                        <TextField type="text" label="Enter first name" variant="outlined" className="inputBox" name="accountantFName" required onChange={this.handleChange}></TextField> &nbsp;
+                        <TextField type="text" label="Enter last name" variant="outlined" className="inputBox" name="accountantLName" required onChange={this.handleChange}></TextField>&nbsp;
+                        <TextField type="email" label="Enter email address" variant="outlined" className="inputBox" name="accountantEmailAddress" style={{ marginBottom: "20px" }}
+                         required onChange={this.handleChange}></TextField>&nbsp;
+
+                       <p>Date of birth <TextField variant="outlined" type="date" InputLabelProps={{ shrink: true }} style={{ marginBottom: "20px" }} name="accountantDOB" onChange={this.handleChange} /></p> &nbsp;
+                        &nbsp; &nbsp;
+
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">
+                                Gender &nbsp;
+                            </FormLabel>
+                            <RadioGroup aria-label="gender" name="gender" value={this.value} onChange={this.handleChange} row name="gender">
+                                <FormControlLabel value="Female" control={<Radio />} label="Female" />
+                                <FormControlLabel value="Male" control={<Radio />} label="Male" />
+                                <FormControlLabel value="Other" control={<Radio />} label="Other" />
+                            </RadioGroup>
+                        </FormControl>
+
+                        <TextField label="Enter your experience(in years)" variant="outlined" className="inputBox" type="number" onChange={this.handleChange}
+                            name="accountantExperience"></TextField>&nbsp;
+
+                        <TextField label="Enter your mobile number" variant="outlined" className="inputBox" type="number" onChange={this.handleChange}
+                            name="accountantMobNo"></TextField>&nbsp;
+
+                            
+                        <TextField label="Quote your price(per day)" variant="outlined" className="inputBox" type="number" onChange={this.handleChange}
+                            name="pricePerDay"></TextField>&nbsp;
+
+                        <TextField label="Type your password" variant="outlined" className="inputBox" type="password" onChange={this.handleChange}
+                            name="accountantPassword" required style={{ marginBottom: "20px" }}></TextField>&nbsp;
+
+                            <p><Input type="file" color="primary" style={{ marginBottom: "20px" }} name="accountantImage" onChange={this.handleFile} >Upload your image</Input></p>
+
+                        <Button variant="contained" color="primary" className="submitBtn" onClick={this.registerAccountant}>Register</Button>
+
+                        <Link to='/register'><p><u>Click here to register as a user</u></p></Link>
+                    </form>
+                </div>
+
             </div>
+        </div>
+    </div>
         )
     }
 }
