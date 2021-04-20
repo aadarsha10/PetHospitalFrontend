8e6b57532ea57d82e20c204dab33581b14792149
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2021-04-20 14:15:10 +0545

    using hooks in login2

--- src/Authentication/LoginUser/Login.js
+++ src/Authentication/LoginUser/Login.js
@@ -1,9 +1,55 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react'
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import '../scss/Register.scss'
 
-class Login extends Component{
-    render(){
-    return (
-        <div>
+
+const Login = ({history}) =>{
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [checkLogin, setCheckLogin] = useState(false);
+
+  const loginstart = (e)=>{
+        e.preventDefault();
+        console.log(username,password)
+        const data = {
+            username : username,
+            password : password
+        }
+        
+        // token:token,
+        // Username:userData.userName,
+        // Email:userData.email,
+        // Lastname:userData.lastName,
+        // Firstname:userData.firstName
+        axios.post("http://localhost:90/login", data)
+        .then((response)=>{
+            console.log(response);
+            localStorage.setItem('token', response.data.token)
+            localStorage.setItem('fname', response.data.fname)
+            localStorage.setItem('lname', response.data.lname)
+            localStorage.setItem('username', response.data.username)
+            localStorage.setItem('email', response.data.email)
+           setCheckLogin(true)
+           console.log("username", localStorage.getItem('username'))
+        })        
+        .catch((err)=>{
+            console.log(err.response)
+        })
+    }
+
+
+
+    const goToLanding = () => {
+        history.push("/landing");
+    }
+
+        return(
+
+            <div>
+                {
+            !checkLogin &&
+            <div>
                 <div className="container">
                     <h1 className="text-center">Login</h1>
 
@@ -12,21 +58,27 @@ class Login extends Component{
                         <div className='col-md-6' id="regBox">
                             <form id="registrationForm" method="POST">
                                
-                                <TextField type="text" label="Enter email address" variant="outlined" className="inputBox" name="emailAddress" value={this.state.emailAddress} onChange={this.handleChange}></TextField>&nbsp;
+                                <TextField type="text" label="Enter username" variant="outlined" className="inputBox" name="username" value ={username} onChange={(event)=> {return setUsername(event.target.value)}}></TextField>&nbsp;
 
-                                <TextField label="Type your password" variant="outlined" className="inputBox" type="password" value={this.state.password} onChange={this.handleChange}
+                                <TextField label="Enter your password" variant="outlined" className="inputBox" type="password" value ={password} onChange={(event)=> {return setPassword(event.target.value)}}
                                   type="password"  name="password" style={{ marginBottom: "20px" }}></TextField>&nbsp;
 
-                                <Button variant="contained" color="primary" className="submitBtn" onClick={this.login}>Login</Button>
+                                <Button variant="contained" color="primary" className="submitBtn" onClick={loginstart}>Login</Button>
                             </form>
 
-                            <Link to='/accountantLogin'><p><u>Click here to sign in as an accountant</u></p></Link>
+                            <Link to='/admin login'><p><u>Sign In as Admin</u></p></Link>
                         </div>
 
                     </div>
                 </div>
             </div>
+}
+            {
+                checkLogin &&
+                goToLanding()
+            }
+            </div>
         )
     }
-}
+
 export default Login;
